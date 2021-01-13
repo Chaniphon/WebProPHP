7e899ea07b8ce169145d14e75cd78c586349b0d0
AomsinChaniporn <620602120086@fitm.kmutnb.ac.th>
2021-01-13 09:58:04 +0700

    update

---
+++ hello.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>
+            Document
+        </title>
+    </head>
+    <body>
+        <?php
+        echo "<h1>Hello</h1><br>";
+        echo "<h2>Prachiburi</h2>";
+        ?>
+        
+
+    </body>
+</html>
