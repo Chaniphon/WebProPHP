76519e1e8b07aca221acf0c0ab8fa8f69311c792
AomsinChaniporn <620602120086@fitm.kmutnb.ac.th>
2021-01-13 11:26:49 +0700

    update

---
+++ lab4-2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab4.2</title>
+</head>
+<body>
+    <?php
+        $x = 123;
+        $y = "abc";
+        echo "$ x = "; echo $x; echo "<br />";
+        echo "$ y = "; echo $y; echo "<br />";
+
+        $x = &$y;   //& : pass by reference
+        echo "$ x = &$ y <br />";
+        echo "$ x = "; echo $x; echo "<br />";
+        echo "New value $ x<br />";
+
+        $x = "php";     //ให้ค่าใหม่กับx จากเดิมเป็นabc
+        echo "$ x = "; echo $x; echo "<br />";
+        echo "$ y = "; echo $y; echo "<br />";
+        echo "New value $ y<br />";
+
+        $y = 100;
+        echo "$ x = "; echo $x; echo "<br />";
+        echo "$ y = "; echo $y; echo "<br />";
+    ?>
+</body>
+</html>
