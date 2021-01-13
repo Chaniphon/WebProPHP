3d5809cf2158236c5acde26567b9a3a61562ba29
AomsinChaniporn <620602120086@fitm.kmutnb.ac.th>
2021-01-13 10:58:42 +0700

    update

---
+++ lab4-1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab4.1</title>
+</head>
+<body>
+    <?php
+        $x = 100;
+        $y = "200";
+        $z = '300';
+        $add = $x + $y;
+        $sub = $z - $x;
+        $multi = $y * $z;
+        $divide = $multi / $z;
+        $message = "Hello World";
+
+        echo $x; echo "<br />";
+        echo $y; echo "<br />";
+        echo $z; echo "<br />";
+        echo $add; echo "<br />";
+        echo $sub; echo "<br />";
+        echo $multi; echo "<br />";
+        echo $divide; echo "<br /";
+        echo $message; echo "<br />";
+    ?>
+</body>
+</html>
